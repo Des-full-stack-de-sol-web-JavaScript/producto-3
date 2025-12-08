d1fcd20075e5166350940af20c220fd120df4022
Thatatadm <mthabatadenise@gmail.com>
2025-12-08 12:10:54 +0100

    Actualización de conexion MongoDB para usar Atlas

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,6 +1,7 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
-const uri = 'mongodb://admin:pass@localhost:27017/producto3?authSource=admin';
+const uri =
+  "mongodb+srv://db_user:pass123@cluster0.yvuhuz9.mongodb.net/productosDB?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
@@ -8,21 +9,23 @@ let db;
 
 export async function connectDB() {
   try {
-    console.log('Conectando a MongoDB...');
+    console.log("Conectando a MongoDB Atlas...");
     await client.connect();
 
-    db = client.db('producto3');
+    db = client.db("productosDB");
 
-    console.log('✅ Conexión a MongoDB establecida.');
+    console.log("✅ Conexión establecida a Atlas.");
   } catch (error) {
-    console.error('❌ No se pudo conectar a MongoDB', error);
+    console.error("❌ Error conectando a MongoDB Atlas", error);
     process.exit(1);
   }
 }
 
 export function getDB() {
   if (!db) {
-    throw new Error('La base de datos no está conectada. Llama a connectDB primero.');
+    throw new Error(
+      "La base de datos no está conectada. Llama a connectDB primero."
+    );
   }
   return db;
 }
